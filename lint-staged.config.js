f9592fe58e8b030e3a930c152daadd70f005f41e
waldronmatt <mhuntwaldron@gmail.com>
2022-11-26 16:23:34 -0500

    fix(lint-staged.config.js): add lint staged config for yml files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
+const ymlConfig = require('@waldronmatt/lint-staged-config/yml');
+
 module.exports = {
   '*': ['secretlint'],
   '**/*': ['prettier --cache --write --ignore-unknown'],
   '*.md': ['markdownlint'],
-  '*.{yml,yaml}': ['yamllint --strict .'],
-  '.github/workflows/*.{yml,yaml}': ['actionlint'],
+  ...ymlConfig,
 };
